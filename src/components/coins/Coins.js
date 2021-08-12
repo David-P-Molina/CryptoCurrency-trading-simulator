4291fd2effca05f9296b8086c060d2f59ced1a73
David Moina <davemolina777@gmail.com>
2021-08-11 19:22:33 -0500

    Create basic coins component

---
+++ src/components/coins/Coins.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Coin from './Coin'
+
+export default class Coins extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
