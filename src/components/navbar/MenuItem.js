41cd66ce1c23edd2fe6a477fafd65a849d3e150a
David Moina <davemolina777@gmail.com>
2021-08-12 13:43:15 -0500

    Create menu items for navbar

---
+++ src/components/navbar/MenuItem.js
@@ -0,0 +1,34 @@
+const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    }, 
+    {
+        title: 'Coins',
+        url: '#',
+        cName: 'nav-links'
+    }, 
+    {
+        title: 'Wallet',
+        url: '#',
+        cName: 'nav-links'
+    }, 
+    {
+        title: 'Learn More',
+        url: '#',
+        cName: 'nav-links'
+    }, 
+    {
+        title: 'Sign-In/Login',
+        url: '#',
+        cName: 'nav-links'
+    },     
+    {
+        title: 'Sign-Up',
+        url: '#',
+        cName: 'nav-links'
+    }, 
+]
+
+export default MenuItems
