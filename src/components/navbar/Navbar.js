64701d7ff51a81d7cb3dd2c3865b6cb54dfbf85b
David Moina <davemolina777@gmail.com>
2021-08-12 13:42:19 -0500

    Add main title, ul and li

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,7 +4,10 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="NavbarItems">
-
+                <h1 className="app-name">MemeCoin Simulator</h1>
+                <ul>
+                    <li><a href=""></li>
+                </ul>
             </nav>
          )
     }
