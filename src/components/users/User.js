f1783874b93215c43f520181160b0cfb27b8cb02
David Moina <davemolina777@gmail.com>
2021-08-11 19:56:26 -0500

    Create basic user class component and import wallet

---
+++ src/components/users/User.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Wallet from './wallet'
+
+export default class User extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
