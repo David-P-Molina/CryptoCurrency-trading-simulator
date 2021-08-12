833e1164df95e1fe0ecb609ccf6ddaf8827ace1a
David Moina <davemolina777@gmail.com>
2021-08-11 19:52:22 -0500

    Add value input and add attribute to form and input tags

--- src/components/wallets/WalletInput.js
+++ src/components/wallets/WalletInput.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 
 class WalletInput extends Component {
     state = {
-        name: ''
+        name: '',
+        value: 10000
     }
-    
+
     handleOnSubmit = event => {
         event.preventDefault()
         this.props.(this.state.name) //Finish add wallet information
@@ -16,12 +17,19 @@ class WalletInput extends Component {
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleOnSubmit}>
                     Create A New Wallet
                     <label>Wallet Nickname:</label>
                     <input
                         type="text"
-                        value={this.state.text}
+                        value={this.state.name}
+                        onChange={this.handleOnChange}
+                        />
+                    <label>Starting Amount</label>
+                    <input
+                        type="number"
+                        value={this.state.value}
+                        onChange={this.handleOnChange}
                         />
                     <input type="submit" />
                 </form>
