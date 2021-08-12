1fa468d421a4126eda54e70e337b90bd5231432a
David Moina <davemolina777@gmail.com>
2021-08-11 20:03:09 -0500

    Create basic wallet container

---
+++ src/containers/WalletContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class WalletContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default WalletContainer
