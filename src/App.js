ad0880804732d800508f71c5a2a4433a658f1622
David Moina <davemolina777@gmail.com>
2021-08-12 20:30:27 -0500

    create base home route information

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-
+import Home from './components/home'
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <Route path="/">
+          <Home />
+        </Route>
       </Router>
     </div>
   );
