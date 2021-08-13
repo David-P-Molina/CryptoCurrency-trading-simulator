4f7fdf94d7de88dec64812795f6c323a443b9d3a
David Moina <davemolina777@gmail.com>
2021-08-13 09:07:55 -0500

    Add basic redux introduction to app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
 
+const store = createStore()
 ReactDOM.render(
   <React.StrictMode>
     <App />
