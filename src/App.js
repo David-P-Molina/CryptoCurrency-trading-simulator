80fdde37cb176d66a946806679fe07a857f242a4
David Moina <davemolina777@gmail.com>
2021-08-12 20:49:55 -0500

    create basic learnmore component and add routes for other paths

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/navbar/Navbar'
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from './components/Home'
+import LearnMore from './components/LearnMore'
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,24 @@ function App() {
         <Route path="/">
           <Home />
         </Route>
+        {/* <Route path="/coins">
+          <CoinContainer />
+        </Route>
+        <Route path="/my-wallet">
+          <WalletContainer />
+        </Route> */}
+        <Route path="/learn-more">
+          <LearnMore />
+        </Route>
+        {/* <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/logout">
+          <Logout />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route> */}
       </Router>
     </div>
   );
---
+++ src/components/LearnMore.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LearnMore() {
+    return (
+        <div>
+            <h1>What is Cryptocurrency</h1>
+            <h1>Why memecoins?</h1>
+        </div>
+    )
+}
+
+export default LearnMore
