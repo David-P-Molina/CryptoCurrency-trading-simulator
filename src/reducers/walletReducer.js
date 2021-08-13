7eed0f5be7eb94ff74cb1ba90f34bcb976239377
David Moina <davemolina777@gmail.com>
2021-08-13 09:16:41 -0500

    Create basic reducer format

---
+++ src/reducers/walletReducer.js
@@ -0,0 +1,9 @@
+const walletReducer = (state = [], action) => {
+    switch(action.type) {
+        case '':
+            return []
+        default:
+            return state
+    }
+}
+export walletReducer
