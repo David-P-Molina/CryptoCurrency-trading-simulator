59f392e1027896588c0152b3a0287a8e613b2899
David Moina <davemolina777@gmail.com>
2021-08-17 11:37:26 -0500

    Add basic framework for inputs

--- src/components/users/Signup.js
+++ src/components/users/Signup.js
@@ -21,10 +21,28 @@ class Signup extends Component {
         return (
             <div>
                 <form onChange={this.handleOnSubmit}>
+                    <h1>Sign Up to Create Your Wallet</h1>
                 <label>Username:</label>
+                <input 
+                    type="" 
+                    value={this.state.} 
+                    onChange={this.handleOnChange} />
                 <label>Email:</label>
+                <input 
+                    type="" 
+                    value={this.state.} 
+                    onChange={this.handleOnChange} />
                 <label>Password:</label>
+                <input 
+                    type="" 
+                    value={this.state.} 
+                    onChange={this.handleOnChange} />
                 <label>Confirm Password:</label>
+                <input 
+                    type="" 
+                    value={this.state.} 
+                    onChange={this.handleOnChange} />
+                <input type="submit" value="signup" />
                 </form>
             </div>
         )
