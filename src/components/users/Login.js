bf040035abd15f66e1a3a24545517f02542035de
David Moina <davemolina777@gmail.com>
2021-08-17 12:00:45 -0500

    Remove double export from file

--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-export class Login extends Component {
+class Login extends Component {
     state = {
         username: "",
         password: "",
