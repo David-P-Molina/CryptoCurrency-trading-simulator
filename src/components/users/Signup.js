e392df98910002fdca448cf3d3ca83642c163a59
David Moina <davemolina777@gmail.com>
2021-08-17 11:28:22 -0500

    Add basic handleOnSubmit method

--- src/components/users/Signup.js
+++ src/components/users/Signup.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react'
 class Signup extends Component {
     state = {
         username: "",
+        email: "",
         password: "",
     }
-    
+    handleOnSubmit = (e) => {
+        e.preventDefault()
+        const { username, email, password } = this.state
+    }
+
     render() {
         return (
             <div>
