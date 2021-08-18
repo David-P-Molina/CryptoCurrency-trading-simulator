ac2a1c00f6505054748fa731197bbc4845cb4f89
David Moina <davemolina777@gmail.com>
2021-08-18 13:58:54 -0500

    Convert navbar class to const and format lines

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 import MenuItems from './MenuItem'
 import { NavLink } from 'react-router-dom'
 import './Navbar.css'
 
-class Navbar extends Component {
-    render() {
-        return (
-            <nav className="NavbarItems">
-                <h1 className="app-name">MemeCoin Simulator</h1>
-                <ul className="nav-menu">
-                    {MenuItems.map((item, index) => {
-                        return (
-                            <NavLink
-                                to={item.url}
-                                key={index}
-                                className={item.cName}
-                            >
-                                    {item.title}
-                            </NavLink>
-                        )
-                    })}
-                </ul>
-            </nav>
-         )
-    }
+const Navbar =({ authChecked, LoggedIn, currentUser}) => {
+    return (
+        <nav className="NavbarItems">
+            <h1 className="app-name">MemeCoin Simulator</h1>
+            <ul className="nav-menu">
+            {MenuItems.map((item, index) => {
+                return (
+                    <NavLink
+                        to={item.url}
+                        key={index}
+                        className={item.cName}
+                    >
+                        {item.title}
+                        </NavLink>
+                    )
+                })}
+            </ul>
+        </nav>
+     )
 }
 
 export default Navbar;
