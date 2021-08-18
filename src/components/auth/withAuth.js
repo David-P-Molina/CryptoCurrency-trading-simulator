d2b0e35e26951bb7102243085ce4afb855dea7ff
David Moina <davemolina777@gmail.com>
2021-08-18 15:34:07 -0500

    Create base wrapped component withAuth

---
+++ src/components/auth/withAuth.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { checkAuth } from '../../actions/auth'
+import Login from './users/Login'
+
+function withAuth(WrappedComponent) {
+    class Wrapper extends React.Component {
+        
+        render() {
+
+        }
+    }
+}
+
+export default withAuth
