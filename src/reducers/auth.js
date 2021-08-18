bfb3b103ca02fcebaefdc78639062bf9480ba789
David Moina <davemolina777@gmail.com>
2021-08-18 11:08:06 -0500

    Create authreducer logic

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -5,3 +5,25 @@ const initialState ={
     loggedIn: false,
     currentUser: {}
 }
+
+function authReducer(state = initialState, action) {
+    switch (action.type) {
+        case AUTHENTICATED:
+            return {
+                authChecked: true,
+                loggedIn: true,
+                currentUser: action.payload,
+            }
+        case NOT_AUTHENTICATED: 
+            return {
+                authChecked: true,
+                loggedIn: false,
+                currentUser: {}
+            }
+        default : 
+        return state
+    }
+}
+
+
+export default authReducer
