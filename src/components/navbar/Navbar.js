50fa05cfa876dc8578aa73c7418714601a90aad4
David Moina <davemolina777@gmail.com>
2021-08-18 20:42:19 -0500

    fix typos and include if else logic for user functionality

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import { connect } from 'react-redux'
-// import MenuItems from './MenuItem'
 import { NavLink } from 'react-router-dom'
+import Logout from "../users/Logout"
 import './Navbar.css'
 
-const Navbar = ({ authChecked, LoggedIn, currentUser }) => {
+const Navbar = ({ authChecked, loggedIn, currentUser }) => {
     return (
         <nav className="NavbarItems">
             <h1 className="app-name">MemeCoin Simulator</h1>
             <ul className="nav-menu">
                 <NavLink to="/" className="nav-links" >Home</NavLink>            
                 <NavLink to="/coins" className="nav-links" >Coin</NavLink>            
-                <NavLink to="/wallet" className="nav-links" >Wallet</NavLink>        
-                <NavLink to="/signup" className="nav-links" >Sign Up</NavLink>
-                <NavLink to="/login" className="nav-links" >Login</NavLink>
-                <NavLink to="/logout" className="nav-links" >Logout</NavLink>
-            </ul>
+                <NavLink to="/wallet" className="nav-links" >Wallet</NavLink> 
+                <NavLink to="/learn-more" className="nav-links" >Learn More</NavLink>       
+                {loggedIn ? (
+                    <>
+                        {currentUser.username}
+                        <Logout />
+                    </>
+                ) : (
+                    <>
+                        <NavLink to="/signup" className="nav-links" >Sign Up</NavLink>
+                        <NavLink to="/login" className="nav-links" >Login</NavLink>
+                    </>
+                )}
+                </ul>
         </nav>
      )
 }
