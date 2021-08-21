7a71d14e30d49e798f33ab14f2ed5bbdba9fc36e
David Moina <davemolina777@gmail.com>
2021-08-20 20:21:17 -0500

    Create getToken function

--- src/actions/wallet.js
+++ src/actions/wallet.js
@@ -1,5 +1,12 @@
 import { CREATE_WALLET } from "."
-
+const getToken = () => {
+    const now = new Date(Date.now()).getTime();
+    const timeAllowed = 1000 * 60 * 30;
+    const timeSinceLastLogin = now - localStorage.getItem("lastLoginTime")
+    if (timeSinceLastLogin < timeAllowed) {
+        return localStorage.getItem("token")
+    }
+}
 export function createWallet(walletInfo) {
     return (dispatch) => {
         return fetch("http://localhost:3001/wallets"), {
