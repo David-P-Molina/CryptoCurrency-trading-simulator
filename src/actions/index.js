16e0c35e9057d8547a47a37a4152c694c34286a2
David Moina <davemolina777@gmail.com>
2021-08-21 12:07:37 -0500

    Add error and database action to index

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,3 +10,7 @@ export const BUY_COIN = 'BUY_COIN'
 export const ADD_COINS = 'ADD_COINS'
 export const FETCH_INITIAL_COINS_INFO = 'FETCH_INITIAL_COINS_INFO'
 export const UPDATE_COIN_VALUES = 'UPDATE_COIN_VALUES'
+//Errors
+export const ERROR = 'ERROR'
+//Database
+export const ADD_TO_DATABASE = 'ADD_TO_DATABASE'
