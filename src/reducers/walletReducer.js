86c113fead3ee0c1dcb32c45241d83314f022ec9
David Moina <davemolina777@gmail.com>
2021-08-21 12:24:11 -0500

    Add basic layout for wallet reducer

--- src/reducers/walletReducer.js
+++ src/reducers/walletReducer.js
@@ -1,9 +1,13 @@
+import { CREATE_WALLET, DELETE_WALLET, SELL_COIN, BUY_COIN, ERROR, ADD_TO_DATABASE } from "../actions"
+
 const walletReducer = (state = {wallet: [], loading: false, error: ""}, action) => {
     switch(action.type) {
         case ADD_TO_DATABASE:
             return {...state, loading: action.payload}
         case ERROR:
             return {...state, error: action.payload}
+        case CREATE_WALLET:
+            return {...state, wallet: action.payload}
         default:
             return state
     }
